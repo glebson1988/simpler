39597ca8e590dba280ff4f565f932d0f51fda3ed
glebson1988 <glebson1988@yandex.ru>
2019-08-06 10:04:46 +0300

    homework of lesson20 att1

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,9 +2,27 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    #set_status 302
+    #set_headers_type :json
+    #render plain: "Text plain test\n
+    #
+    # render json: {
+    #     "firstName": "Иван",
+    #     "lastName": "Иванов",
+    #     "address": {
+    #         "streetAddress": "Московское ш., 101, кв.101",
+    #         "city": "Ленинград",
+    #         "postalCode": "101101"
+    #     },
+    #     "phoneNumbers": [
+    #         "812 123-1234",
+    #         "916 123-4567"
+    #     ]
+    # }
   end
 
-  def create; end
+  def create;
+  end
 
   def show
     @params = params
